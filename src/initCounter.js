04011b91a3f4cd4822b499a4c74d92cd31de91c8
Sawdes <lawerdaniil90@gmail.com>
2024-07-22 00:15:58 +0300

    add initCounter function

---
+++ src/initCounter.js
@@ -0,0 +1,33 @@
+// Exmaple usages:
+// const [increment, decrement] = makeCounter();
+// increment(); // 1
+
+// Exmaple usages with state var and step:
+// let state = 2
+// const step = 5
+// const [, decrement] = initCounter(state, step);
+// decrement(); // -3
+
+function initCounter(stateVar = 0, step = 1) {
+  const increment = (newStep = false) => {
+    // If user want change step
+    if (newStep) step = newStep;
+
+    // Change state
+    stateVar = stateVar + step;
+    // return state variable
+    return stateVar;
+  };
+
+  const decrement = (newStep = false) => {
+    // If user want change step
+    if (newStep) step = newStep;
+    // Change state
+    stateVar = stateVar - step;
+    // return state variable
+    return stateVar;
+  };
+
+  // return funcitons set
+  return [increment, decrement];
+}

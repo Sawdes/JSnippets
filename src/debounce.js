5705db7da2bc46032e5fa95f3ba0952aa857117d
Sawdes <lawerdaniil90@gmail.com>
2024-07-24 14:20:02 +0300

    add function debouce

---
+++ src/debounce.js
@@ -0,0 +1,17 @@
+// Example for uages
+// const debouncedLog = debounce(console.log, 100);
+// debouncedLog("Hello world") // cancelled
+// debouncedLog("Hello world2") // cancelled
+// debouncedLog("Hello world3") // cancelled
+// debouncedLog("Hello world4") // Hello world4 after 100ms
+
+function debounce(fn, t) {
+  let timerId; // Init var to hold the timeout ID
+
+  return (...args) => {
+    // Return a new function that wraps the original function
+    clearTimeout(timerId); // Clear any existing timer to reset the debounce delay
+    // Set a new timer that will call 'fn' with the provided arguments after the specified delay 't'
+    timerId = setTimeout(() => fn(...args), t);
+  };
+}
